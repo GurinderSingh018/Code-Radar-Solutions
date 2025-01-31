68d76751e56abcb854ccb7c90f293912e2efe05d
GurinderSingh018 <gurinder0295.becse24@chitkara.edu.in>
2025-01-31 21:51:09 +0800

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -12,7 +12,12 @@ int main() {
     printf("%d",a-b);
    }
    else if(o=='/'){
+    if(b==0){
+        printf("error");
+    }
+    else{
     printf("%d",a/b);
+    }
    }
    else if(o=='*'){
     printf("%d",a*b);
