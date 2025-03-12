fdbade5ee208d3705c93cb79a1946fa4806d6120
GurinderSingh018 <gurinder0295.becse24@chitkara.edu.in>
2025-03-12 20:23:37 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
 #include<string.h>
-void remove_newline(char *str) {
-    size_t len = strlen(str);
-    if (len > 0 && str[len - 1] == '\n') {
-        str[len - 1] = '\0'; 
-    }
-}
-int main(){
-    char str[50];
-    char str2[50];
-    fgets(str,sizeof(str),stdin);
-    fgets(str2,sizeof(str2),stdin);
+
+void sort(char str[],n){
     
-    int valid=0;
-    for(int i=0;i<strlen(str);i++){
-        for(int j=0;j<strlen(str2);j++){
-            if(str[i]==str2[j]){
-                valid=1;
-                break;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;i<n;i++){
+            if(arr[i]>arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
             }
         }
-        if(valid==0){
-            break;
+    }
+}
+
+int main(){
+    char str1[50];
+    char str2[50];
+    int n=strlen(str1);
+    fgets(str1,sizeof(str1),stdin);
+    fgets(str2,sizeof(str2),stdin);
+    sort(str1,n);
+    sort(str2,n);
+    int valid=0;
+    for(int i=0;i<n;i++){
+        if(str1[i]==str2[i]){
+            valid=1;
         }
     }
     if(valid){
@@ -29,6 +33,5 @@ int main(){
     }
     else{
         printf("No");
-}
-    return 0;
+    }
 }
