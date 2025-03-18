96c3084b244eb363a759f803c4e100316f21ec76
GurinderSingh018 <gurinder0295.becse24@chitkara.edu.in>
2025-03-18 21:56:04 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct Vehicle{
+    char num[10];
+    char type[10];
+    int amount;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    struct Vehicle veh[n];
+    for(int i=0;i<n;i++){
+        scanf("%s %s %d",veh[i].num,veh[i].type,&veh[i].amount);
+    }
+    float X=0;Y=0,Z=0;
+    for(int i=0;i<n;i++){
+        if(veh[i].type=='Car'){
+            X=X+veh[i].amount;
+        }
+        else if(veh[i].type=='Bike'){
+            Y=Y+veh[i].amount;
+        }
+        else{
+            Z=Z+veh[i].amount;
+        }
+    }
+    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",X,Y,Z);
+    return 0;
+}
