3c5d7127f1373413b877e08107dba3d8150c51f0
GurinderSingh018 <gurinder0295.becse24@chitkara.edu.in>
2025-03-20 20:30:59 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -5,17 +5,19 @@ void selectionSort(char arr[][100],int n){
     for(int i=0;i<n-1;i++){
         int mini=i;
         for(int j=i+1;j<n;j++){
-            if(strcmp(arr[j],arr[mini])<0){
-                mini=j;
-                
+            if(strcmp(arr[j],arr[j-1])<0){
+                char temp[100];
+                strcpy(temp,arr[j-1]); 
+        strcpy(arr[i],arr[j-1]);
+        strcpy(arr[j-1],temp);               
             }
         }
-        if(mini!=i){
-        char temp[100];
-        strcpy(temp,arr[i]); 
-        strcpy(arr[i],arr[mini]);
-        strcpy(arr[mini],temp);
-        }
+        // if(mini!=i){
+        // char temp[100];
+        // strcpy(temp,arr[i]); 
+        // strcpy(arr[i],arr[mini]);
+        // strcpy(arr[mini],temp);
+        // }
     }
 }
 
