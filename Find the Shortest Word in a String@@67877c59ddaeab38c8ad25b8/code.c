70db41bda466cd0b555c12abd9f459f4743f31bc
GurinderSingh018 <gurinder0295.becse24@chitkara.edu.in>
2025-04-05 22:00:01 +0530

    Solution to Find the Shortest Word in a String

---
+++ Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    char str[50],str2[10][50];
+    scanf("%[^\n]s",&str);
+    int j=0,k=0;
+    for(int i=0;i!='\0';i++){
+        if(str[i]==' '){
+            str2[k][j]='\0';
+            k++;
+            j=0;
+        }
+        else{
+            str2[k][j]=str[i];
+            j++;
+        }
+    }
+    str2[k][j]='\0';
+    int m=0;
+    for(int i=0;i<k;i++){
+        if(strlen(str2[i])<stlen(str2[m])){
+            m=i;
+        }
+    }
+    printf("%s",str[m]);
+}
