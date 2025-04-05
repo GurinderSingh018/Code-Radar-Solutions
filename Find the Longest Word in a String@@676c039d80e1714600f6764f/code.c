1d1452f0c41c1a5057d17f4d19d9fef62d3c34b7
GurinderSingh018 <gurinder0295.becse24@chitkara.edu.in>
2025-04-05 16:24:27 +0530

    Solution to Find the Longest Word in a String

--- Find the Longest Word in a String@@676c039d80e1714600f6764f/code.c
+++ Find the Longest Word in a String@@676c039d80e1714600f6764f/code.c
@@ -4,7 +4,7 @@ int main(){
       char str[50];
       char str2[10][50];
       fgets(str,sizeof(str),stdin);
-      
+      str[strcspn(str, "\n")] = '\0';
       int i,j=0,k=0;
       for(i=0;str[i]!='\0';i++){
         if(str[i]==' '){
@@ -13,8 +13,8 @@ int main(){
             k++;
         }
         else{
-        str2[k][j]=str[i];
-        j++;
+          str2[k][j]=str[i];
+          j++;
         }
       }
       str2[k][j]='\0';
